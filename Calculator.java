6094297ec1c5167fadf7539eb4e43c30907b26fb
qwertylch <chlee910@gmail.com>
2023-09-19 15:54:58 +0900

    lch 4th

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,7 @@
 public class Calculator {
     public static double performOperation(double num1, double num2, char operator) {
-// task 1
+// task 2
+
         double result = 0.0;
 
         switch (operator) {
